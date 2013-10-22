18ea649f45ac77494a712a2a8a31cc649c4ec0cf
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-10-22 10:39:36 +0200

    test works after file rename

--- src/test/java/dk/statsbiblioteket/medieplatform/newspaper/SchemaValidatorTest.java
+++ src/test/java/dk/statsbiblioteket/medieplatform/newspaper/SchemaValidatorTest.java
@@ -14,9 +14,9 @@ public class SchemaValidatorTest {
         Validator validator = new SchemaValidator("jpylizer.xsd");
         InputStream jpylizerFile = Thread.currentThread().getContextClassLoader()
                                          .getResourceAsStream(
-                                                 "AdresseContoirsEfterretninger-1795-06-13-01-0006.xml");
+                                                 "valid.xml");
         ResultCollector results = new ResultCollector("test", "0.1");
-        validator.validate("src/test/resources/AdresseContoirsEfterretninger-1795-06-13-01-0006.xml",jpylizerFile,results);
+        validator.validate("valid.xml",jpylizerFile,results);
         System.out.println(results.toReport());
         Assert.assertTrue(results.isSuccess());
 
