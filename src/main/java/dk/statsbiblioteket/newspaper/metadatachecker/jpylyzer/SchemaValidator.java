4fdca231c3027943c3b0ec34689f596bc169ba7f
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-10-23 16:40:35 +0200

    Logging added

--- src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/jpylyzer/SchemaValidator.java
+++ src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/jpylyzer/SchemaValidator.java
@@ -1,6 +1,8 @@
 package dk.statsbiblioteket.newspaper.metadatachecker.jpylyzer;
 
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -14,84 +16,89 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.net.URL;
 
-/**
- * The schema validator. It validates the input against the schema given in the constructor
- */
+/** The schema validator. It validates the input against the schema given in the constructor */
 public class SchemaValidator
         implements Validator {
     private final URL schemaURL;
 
+    private final Logger log = LoggerFactory.getLogger(SchemaValidator.class);
 
     /**
      * Construct a new schema validator. The schema name is found on the classpath.
-     * @param schemaName the classpath address of the schema to use
      *
+     * @param schemaName the classpath address of the schema to use
      */
     public SchemaValidator(String schemaName) {
         schemaURL = Thread.currentThread().getContextClassLoader().getResource(schemaName);
     }
 
-    /**
-     * Get the name of this component for error reporting purposes
-     */
+    /** Get the name of this component for error reporting purposes */
     private String getComponent() {
-        return "JPylizer_schema_validator-"+ getClass().getPackage().getImplementationVersion();
+        return "JPylizer_schema_validator-" + getClass().getPackage().getImplementationVersion();
     }
 
     @Override
     public boolean validate(final String reference,
                             String xml,
                             final ResultCollector resultCollector) {
-        Source xmlFile = new StreamSource(new StringReader(xml));
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema;
+        log.debug("Validating contents of {} via schema {}", reference, schemaURL.toString());
         final boolean[] valid = {true};
         try {
-            schema = schemaFactory.newSchema(schemaURL);
-        } catch (SAXException e) {
-            resultCollector.addFailure(reference, "framework", getComponent(), "Failed to read schema", e.toString());
-            return false;
+            Source xmlFile = new StreamSource(new StringReader(xml));
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema;
+
+            try {
+                schema = schemaFactory.newSchema(schemaURL);
+            } catch (SAXException e) {
+                resultCollector
+                        .addFailure(reference, "framework", getComponent(), "Failed to read schema", e.toString());
+                valid[0] = false;
+                return valid[0];
+            }
+
+            try {
+                javax.xml.validation.Validator validator = schema.newValidator();
+                validator.setErrorHandler(new ErrorHandler() {
+                    @Override
+                    public void warning(SAXParseException exception)
+                            throws
+                            SAXException {
+                        //Ignore
+                    }
+
+                    @Override
+                    public void error(SAXParseException exception)
+                            throws
+                            SAXException {
+                        resultCollector.addFailure(reference, "schemaCheck", getComponent(), exception.toString());
+                        valid[0] = false;
+
+                    }
+
+                    @Override
+                    public void fatalError(SAXParseException exception)
+                            throws
+                            SAXException {
+                        resultCollector.addFailure(reference, "schemaCheck", getComponent(), exception.toString());
+                        valid[0] = false;
+                    }
+                });
+                validator.validate(xmlFile);
+            } catch (SAXException e) {
+                //ignore, the error have already been logged as a fatal error
+            } catch (IOException e) {
+                resultCollector.addFailure(reference, "schemaCheck", getComponent(), e.toString());
+                valid[0] = false;
+            }
+
+            return valid[0];
+        } finally {
+            if (!valid[0]) {
+                log.warn("Failed validation of {} via schema {}", reference, schemaURL.toString());
+            }
         }
-
-        try {
-            javax.xml.validation.Validator validator = schema.newValidator();
-            validator.setErrorHandler(new ErrorHandler() {
-                @Override
-                public void warning(SAXParseException exception)
-                        throws
-                        SAXException {
-                    //Ignore
-                }
-
-                @Override
-                public void error(SAXParseException exception)
-                        throws
-                        SAXException {
-                    resultCollector.addFailure(reference, "schemaCheck", getComponent(), exception.toString());
-                    valid[0] = false;
-
-                }
-
-                @Override
-                public void fatalError(SAXParseException exception)
-                        throws
-                        SAXException {
-                    resultCollector.addFailure(reference, "schemaCheck", getComponent(), exception.toString());
-                    valid[0] = false;
-                }
-            });
-            validator.validate(xmlFile);
-        } catch (SAXException e) {
-            //ignore, the error have already been logged as a fatal error
-        } catch (IOException e) {
-            resultCollector.addFailure(reference, "schemaCheck", getComponent(), e.toString());
-            valid[0] = false;
-        }
-
-        return valid[0];
     }
-
-
 
 
 }
