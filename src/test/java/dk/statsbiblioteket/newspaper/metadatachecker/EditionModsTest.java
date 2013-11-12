430088fb882c3d8a3629cdb1e53acd5d0ab89f25
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-12 12:08:54 +0100

    Fixed the edition mods check

--- src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/EditionModsTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/EditionModsTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
 
 /**
  * Tests that the checks that the metadata for the edition follows the specification
@@ -27,7 +28,7 @@ public class EditionModsTest {
             @Override
             public InputStream getData() throws IOException {
                 return Thread.currentThread().getContextClassLoader().getResourceAsStream(
-                        "scratch/B400022028241-RT1/400022028241-14/1795-06-13-01/AdresseContoirsEfterretninger-1795-06-13-01.edition.xml");
+                        "goodData/adresseavisen1759-1795-06-13-01.edition.xml");
             }
 
             @Override
@@ -36,12 +37,35 @@ public class EditionModsTest {
             }
         };
         handler.handleAttribute(editionEvent);
-        System.out.println(resultCollector.toReport());
+
         assertTrue(resultCollector.isSuccess());
     }
 
+
     /**
-     * TODO Test that we catch invalid edition xml (mods) files
-     */
+       * Test that we can validate a valid edition xml (mods) file.
+       */
+      @Test
+      public void testEditionModsBad() {
+          ResultCollector resultCollector = new ResultCollector("foo", "bar");
+          SchematronValidatorEventHandler handler = new SchematronValidatorEventHandler(resultCollector, null);
+          AttributeParsingEvent editionEvent = new AttributeParsingEvent(
+                  "B400022028241-RT1/400022028241-14/1795-06-13-01/AdresseContoirsEfterretninger-1795-06-13-01.edition.xml") {
+              @Override
+              public InputStream getData() throws IOException {
+                  return Thread.currentThread().getContextClassLoader().getResourceAsStream(
+                          "badData/adresseavisen1759-1795-06-13-01.edition.xml");
+              }
+
+              @Override
+              public String getChecksum() throws IOException {
+                  return null;
+              }
+          };
+          handler.handleAttribute(editionEvent);
+
+          assertFalse(resultCollector.isSuccess());
+
+      }
 
 }
