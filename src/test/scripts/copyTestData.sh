0edfcd72f5540ff0509839569c58821c2c799fd5
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-14 16:07:38 +0100

    Script to copy test data

--- src/test/scripts/copyTestData.sh
+++ src/test/scripts/copyTestData.sh
@@ -2,16 +2,19 @@
 
 scriptDir="$(readlink -f $(dirname $0))"
 
-sourceFolder="$1"
-targetFolder="$scriptDir/../resources/scratch/B400022028241-RT2/"
+sourceFolder=$1
 
-pushd "$targetFolder"
-git rm -fr "$targetFolder"
+targetFolder=$scriptDir/../resources/scratch/B400022028241-RT1/
+
+mkdir -p $targetFolder
+pushd $targetFolder
+git rm -fr $targetFolder
 popd
+mkdir -p $targetFolder
 
-cp -r "$sourceFolder"/* "$targetFolder"
+cp -r $sourceFolder/* $targetFolder
 
-pushd "$targetFolder"
+pushd $targetFolder
 find -name *.jp2 | xargs -I'{}' bash -c "rm  '{}'; touch '{}'; md5sum '{}' > '{}'.md5 "
 find -name *.xml | xargs -I'{}' bash -c "md5sum '{}' > '{}'.md5 "
 
