476569bdc8bcf08c5205ba5f67ce9851d0530259
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-20 12:22:24 +0100

    Mixer mockup can now generate modifiable events so that we can make them bad. This is used to make bad tests for the mix file path checker

--- src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/mix/MixFilePathCheckerTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/mix/MixFilePathCheckerTest.java
@@ -14,90 +14,128 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
 
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class MixFilePathCheckerTest {
 
     private ResultCollector resultCollector = null;
 
-       @BeforeTest
-       public void setUp() {
-           resultCollector = new ResultCollector("test", "test");
-       }
+    @BeforeTest
+    public void setUp() {
+        resultCollector = new ResultCollector("test", "test");
+    }
 
     @Test
-      public void testXpathValidationObjectIdentifierWorkshift() throws ParseException, SQLException, IOException {
-          setUp();
-          final String batchId = "400022028241";
-          final Batch batch = new Batch();
-          batch.setBatchID(batchId);
-          batch.setRoundTripNumber(1);
-          AttributeParsingEvent event2 = MixerMockup.getMixWorkshiftIso(
-                  "000001",
-                  "0001",
-                  batch,
-                  "aac20a9ace772bc5a92b7d7b00048b91",15);
+    public void testXpathValidationObjectIdentifierWorkshift() throws ParseException, SQLException, IOException {
+        setUp();
+        final String batchId = "400022028241";
+        final Batch batch = new Batch();
+        batch.setBatchID(batchId);
+        batch.setRoundTripNumber(1);
+        AttributeParsingEvent event2 = MixerMockup.getMixWorkshiftIso(
+                "000001", "0001", batch, "aac20a9ace772bc5a92b7d7b00048b91", 15);
 
-          Document doc = DOM.streamToDOM(event2.getData());
+        Document doc = DOM.streamToDOM(event2.getData());
 
-          XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
+        XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
 
-          handler.validate(event2,doc);
+        handler.validate(event2, doc);
 
-          String report = resultCollector.toReport();
-          assertTrue(resultCollector.isSuccess(), report);
-      }
+        String report = resultCollector.toReport();
+        assertTrue(resultCollector.isSuccess(), report);
+    }
 
     @Test
-       public void testXpathValidationObjectIdentifier() throws ParseException, SQLException, IOException {
-           setUp();
-           final String batchId = "400022028241";
-           final String film = "1";
-           final String avisID = "adresseavisen1759";
-           final String publishDate = "1795-06-13";
-           final String pictureNumber = "0006";
-           final Batch batch = new Batch();
-           batch.setBatchID(batchId);
-           batch.setRoundTripNumber(1);
-           AttributeParsingEvent event = MixerMockup.getMixPageAttributeParsingEvent(
-                   film, avisID, publishDate, pictureNumber, batch, 9304, 11408, 400, "7ed748249def3bcaadd825ae17dc817a",15);
+    public void testXpathValidationObjectIdentifierWorkshiftBad() throws ParseException, SQLException, IOException {
+        setUp();
+        final String batchId = "400022028241";
+        final Batch batch = new Batch();
+        batch.setBatchID(batchId);
+        batch.setRoundTripNumber(1);
+        MixerMockup.MixWorkshiftIsoAttributeParsingEvent event = MixerMockup.getMixWorkshiftIso(
+                "000001", "0001", batch, "aac20a9ace772bc5a92b7d7b00048b91", 15);
 
-           Document doc = DOM.streamToDOM(event.getData());
+        event.setPictureNumber("0002");
+        Document doc = DOM.streamToDOM(event.getData());
 
-           XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
+        XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
 
-           handler.validate(event,doc);
+        handler.validate(event, doc);
 
-
-           String report = resultCollector.toReport();
-           assertTrue(resultCollector.isSuccess(), report);
-       }
+        String report = resultCollector.toReport();
+        assertFalse(resultCollector.isSuccess(), report);
+    }
 
     @Test
-       public void testXpathValidationObjectIdentifierBad() throws ParseException, SQLException, IOException {
-           setUp();
-           final String batchId = "400022028241";
-           final String film = "1";
-           final String avisID = "adresseavisen1759";
-           final String publishDate = "1795-06-13";
-           final String pictureNumber = "0006";
-           final Batch batch = new Batch();
-           batch.setBatchID(batchId);
-           batch.setRoundTripNumber(1);
-           AttributeParsingEvent event = MixerMockup.getMixPageAttributeParsingEvent(
-                   film, avisID, publishDate, pictureNumber, batch, 9304, 11408, 400, "7ed748249def3bcaadd825ae17dc817a",15);
+    public void testXpathValidationObjectIdentifier() throws ParseException, SQLException, IOException {
+        setUp();
+        final String batchId = "400022028241";
+        final String film = "1";
+        final String avisID = "adresseavisen1759";
+        final String publishDate = "1795-06-13";
+        final String pictureNumber = "0006";
+        final Batch batch = new Batch();
+        batch.setBatchID(batchId);
+        batch.setRoundTripNumber(1);
+        AttributeParsingEvent event = MixerMockup.getMixPageAttributeParsingEvent(
+                film,
+                avisID,
+                publishDate,
+                pictureNumber,
+                batch,
+                9304,
+                11408,
+                400,
+                "7ed748249def3bcaadd825ae17dc817a",
+                15);
 
-           Document doc = DOM.streamToDOM(event.getData());
+        Document doc = DOM.streamToDOM(event.getData());
 
-           XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
+        XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
 
-           handler.validate(event,doc);
+        handler.validate(event, doc);
 
 
-           String report = resultCollector.toReport();
-           assertTrue(resultCollector.isSuccess(), report);
-       }
+        String report = resultCollector.toReport();
+        assertTrue(resultCollector.isSuccess(), report);
+    }
 
+    @Test
+    public void testXpathValidationObjectIdentifierBad() throws ParseException, SQLException, IOException {
+        setUp();
+        final String batchId = "400022028241";
+        final String film = "1";
+        final String avisID = "adresseavisen1759";
+        final String publishDate = "1795-06-13";
+        final String pictureNumber = "0006";
+        final Batch batch = new Batch();
+        batch.setBatchID(batchId);
+        batch.setRoundTripNumber(1);
+
+        MixerMockup.MixPageAttributeParsingEvent event = MixerMockup.getMixPageAttributeParsingEvent(
+                film,
+                avisID,
+                publishDate,
+                pictureNumber,
+                batch,
+                9304,
+                11408,
+                400,
+                "7ed748249def3bcaadd825ae17dc817a",
+                15);
+        event.setPictureNumber(event.getPictureNumber() + 1);
+
+        Document doc = DOM.streamToDOM(event.getData());
+
+        XmlAttributeChecker handler = new MixFilePathChecker(resultCollector);
+
+        handler.validate(event, doc);
+
+
+        String report = resultCollector.toReport();
+        assertFalse(resultCollector.isSuccess(), report);
+    }
 
 
 }
--- src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/mockers/MixerMockup.java
+++ src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/mockers/MixerMockup.java
@@ -9,73 +9,54 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 public class MixerMockup {
-    public static AttributeParsingEvent getMixPageAttributeParsingEvent(final String film, final String avisID,
-                                                                 final String publishDate, 
-                                                                 final String pictureNumber,
-                                                                 final Batch batch,
-                                                                 final int width,
-                                                                 final int height,
-                                                                 final int resolution,
-                                                                 final String checksum,
-                                                                 final int size) {
-        return new AttributeParsingEvent(
+    public static MixPageAttributeParsingEvent getMixPageAttributeParsingEvent(final String film, final String avisID,
+                                                                               final String publishDate,
+                                                                               final String pictureNumber,
+                                                                               final Batch batch, final int width,
+                                                                               final int height, final int resolution,
+                                                                               final String checksum, final int size) {
+        return new MixPageAttributeParsingEvent(
                 batch.getFullID() + "/" +
                 batch.getBatchID() + "-" + film + "/" +
                 publishDate + "-01/" +
-                avisID + "-" + publishDate + "-01-" + pictureNumber + ".mix.xml") {
-            @Override
-            public InputStream getData() throws IOException {
-                return new ByteArrayInputStream(
-                        getCustomPageMix(
-                                batch.getBatchID() + "-" + film, pictureNumber, "2013-11-12T11:48:06",width,height,resolution, checksum,size));
-            }
-
-            @Override
-            public String getChecksum() throws IOException {
-                return null;
-            }
-        };
+                avisID + "-" + publishDate + "-01-" + pictureNumber + ".mix.xml",
+                batch,
+                film,
+                pictureNumber,
+                width,
+                height,
+                resolution,
+                checksum,
+                size);
     }
 
-    public static AttributeParsingEvent getMixWorkshiftIso(final String workshift, final String pictureNumber,
-                                                    final Batch batch,
-                                                    final String checksum,
-                                                    final int size) {
-        return new AttributeParsingEvent(
+    public static MixWorkshiftIsoAttributeParsingEvent getMixWorkshiftIso(final String workshift, final String pictureNumber,
+                                                           final Batch batch, final String checksum, final int size) {
+        return new MixWorkshiftIsoAttributeParsingEvent(
                 batch.getFullID() + "/" +
                 "WORKSHIFT-ISO-TARGET/" +
-                "Target-" + workshift + "-" + pictureNumber + ".mix.xml") {
-            @Override
-            public InputStream getData() throws IOException {
-                return new ByteArrayInputStream(
-                        getCustomPageMix(workshift, pictureNumber, "2013-11-12T11:48:06", 9304, 11408,400,checksum,size));
-            }
-
-            @Override
-            public String getChecksum() throws IOException {
-                return null;
-            }
-        };
+                "Target-" + workshift + "-" + pictureNumber + ".mix.xml", workshift, pictureNumber, checksum, size);
     }
 
-    private static byte[] getCustomPageMix(String filmId, String billedID, String scannedDate, int width, int height, int resolution, String checksum, int size) {
+    private static byte[] getCustomPageMix(String filmId, String billedID, String scannedDate, int width, int height,
+                                           int resolution, String checksum, int size) {
         String mix = "<mix:mix xmlns:mix=\"http://www.loc.gov/mix/v20\">\n" +
                      "    <mix:BasicDigitalObjectInformation>\n" +
                      "        <mix:ObjectIdentifier>\n" +
                      "            <mix:objectIdentifierType>Image Unique ID</mix:objectIdentifierType>\n" +
                      "            <mix:objectIdentifierValue>" + filmId + "-" + billedID + "</mix:objectIdentifierValue>\n" +
                      "        </mix:ObjectIdentifier>\n" +
-                     "        <mix:fileSize>"+size+"</mix:fileSize>\n" +
+                     "        <mix:fileSize>" + size + "</mix:fileSize>\n" +
                      "        <mix:Fixity>\n" +
                      "            <mix:messageDigestAlgorithm>MD5</mix:messageDigestAlgorithm>\n" +
-                     "            <mix:messageDigest>"+checksum+"</mix:messageDigest>\n" +
+                     "            <mix:messageDigest>" + checksum + "</mix:messageDigest>\n" +
                      "            <mix:messageDigestOriginator>Ninestars</mix:messageDigestOriginator>\n" +
                      "        </mix:Fixity>\n" +
                      "    </mix:BasicDigitalObjectInformation>\n" +
                      "    <mix:BasicImageInformation>\n" +
                      "        <mix:BasicImageCharacteristics>\n" +
-                     "            <mix:imageWidth>"+width+"</mix:imageWidth>\n" +
-                     "            <mix:imageHeight>"+height+"</mix:imageHeight>\n" +
+                     "            <mix:imageWidth>" + width + "</mix:imageWidth>\n" +
+                     "            <mix:imageHeight>" + height + "</mix:imageHeight>\n" +
                      "            <mix:PhotometricInterpretation>\n" +
                      "                <mix:colorSpace>greyscale</mix:colorSpace>\n" +
                      "            </mix:PhotometricInterpretation>\n" +
@@ -102,11 +83,11 @@ public class MixerMockup {
                      "        <mix:SpatialMetrics>\n" +
                      "            <mix:samplingFrequencyUnit>in.</mix:samplingFrequencyUnit>\n" +
                      "            <mix:xSamplingFrequency>\n" +
-                     "                <mix:numerator>"+resolution+"</mix:numerator>\n" +
+                     "                <mix:numerator>" + resolution + "</mix:numerator>\n" +
                      "                <mix:denominator>1</mix:denominator>\n" +
                      "            </mix:xSamplingFrequency>\n" +
                      "            <mix:ySamplingFrequency>\n" +
-                     "                <mix:numerator>"+resolution+"</mix:numerator>\n" +
+                     "                <mix:numerator>" + resolution + "</mix:numerator>\n" +
                      "                <mix:denominator>1</mix:denominator>\n" +
                      "            </mix:ySamplingFrequency>\n" +
                      "        </mix:SpatialMetrics>\n" +
@@ -123,6 +104,158 @@ public class MixerMockup {
             return mix.getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new Error("UTF-8 not known");
+        }
+    }
+
+    public static class MixWorkshiftIsoAttributeParsingEvent extends AttributeParsingEvent {
+
+        private String workshift;
+        private String pictureNumber;
+        private String checksum;
+        private int size;
+
+        public MixWorkshiftIsoAttributeParsingEvent(String name, String workshift, String pictureNumber,
+                                                    String checksum, int size) {
+            super(name);
+            this.workshift = workshift;
+            this.pictureNumber = pictureNumber;
+            this.checksum = checksum;
+            this.size = size;
+        }
+
+        @Override
+        public InputStream getData() throws IOException {
+            return new ByteArrayInputStream(
+                    getCustomPageMix(
+                            workshift, pictureNumber, "2013-11-12T11:48:06", 9304, 11408, 400, checksum, size));
+        }
+
+        @Override
+        public String getChecksum() throws IOException {
+            return null;
+        }
+
+        public String getWorkshift() {
+            return workshift;
+        }
+
+        public void setWorkshift(String workshift) {
+            this.workshift = workshift;
+        }
+
+        public String getPictureNumber() {
+            return pictureNumber;
+        }
+
+        public void setPictureNumber(String pictureNumber) {
+            this.pictureNumber = pictureNumber;
+        }
+
+        public void setChecksum(String checksum) {
+            this.checksum = checksum;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+    }
+
+    public static class MixPageAttributeParsingEvent extends AttributeParsingEvent {
+
+        private Batch batch;
+        private String film;
+        private String pictureNumber;
+        private int width;
+        private int height;
+        private int resolution;
+        private String checksum;
+        private int size;
+
+        public MixPageAttributeParsingEvent(String name, Batch batch, String film, String pictureNumber, int width,
+                                            int height, int resolution, String checksum, int size) {
+            super(name);
+            this.batch = batch;
+            this.film = film;
+            this.pictureNumber = pictureNumber;
+            this.width = width;
+            this.height = height;
+            this.resolution = resolution;
+            this.checksum = checksum;
+            this.size = size;
+        }
+
+        @Override
+        public InputStream getData() throws IOException {
+            return new ByteArrayInputStream(
+                    getCustomPageMix(
+                            batch.getBatchID() + "-" + film,
+                            pictureNumber,
+                            "2013-11-12T11:48:06",
+                            width,
+                            height,
+                            resolution,
+                            checksum,
+                            size));
+        }
+
+        public String getFilm() {
+            return film;
+        }
+
+        public void setFilm(String film) {
+            this.film = film;
+        }
+
+        public String getPictureNumber() {
+            return pictureNumber;
+        }
+
+        public void setPictureNumber(String pictureNumber) {
+            this.pictureNumber = pictureNumber;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int getResolution() {
+            return resolution;
+        }
+
+        public void setResolution(int resolution) {
+            this.resolution = resolution;
+        }
+
+        public String getChecksum() {
+            return checksum;
+        }
+
+        public void setChecksum(String checksum) {
+            this.checksum = checksum;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
         }
     }
 }
