14cd3f4762db50035648005e5a91665537c9d587
jrgatsb <jrg@statsbiblioteket.dk>
2013-11-20 10:26:17 +0100

    Unit tests for 2E-2 and 2E-3 schematron film-xml checks

--- src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/FilmValidationTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/metadatachecker/FilmValidationTest.java
@@ -6,18 +6,12 @@ import static org.testng.Assert.assertFalse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import dk.statsbiblioteket.medieplatform.autonomous.Batch;
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
 import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
-import static org.mockito.Mockito.*;
 
 
 public class FilmValidationTest {
@@ -58,7 +52,7 @@ public class FilmValidationTest {
                 "</avis:reelMetadata>\n";
         setUp();
         handleTestEvent(input, resultCollector);
-        assertTrue(resultCollector.isSuccess());
+        assertTrue(resultCollector.isSuccess(), resultCollector.toReport());
     }
 
     @Test
@@ -173,6 +167,76 @@ public class FilmValidationTest {
                 "    <avis:captureResolutionOriginal measurement=\"pixels/inch\">400</avis:captureResolutionOriginal>\n" +
                 "    <avis:captureResolutionFilm measurement=\"pixels/inch\">1024</avis:captureResolutionFilm>\n" +
                 "    <avis:dateMicrofilmCreated>1970-06-05</avis:dateMicrofilmCreated>\n" +
+                "    <avis:resolutionOfDuplicateNegative>6.3</avis:resolutionOfDuplicateNegative>\n" +
+                "    <avis:resolutionCommentDuplicateNegative>No comments</avis:resolutionCommentDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.11</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.13</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:averageDensityDuplicateNegative>0.12</avis:averageDensityDuplicateNegative>\n" +
+                "    <avis:dminDuplicateNegative>0.12</avis:dminDuplicateNegative>\n" +
+                "</avis:reelMetadata>\n";
+        setUp();
+        handleTestEvent(input, resultCollector);
+        assertFalse(resultCollector.isSuccess(), resultCollector.toReport());
+    }
+
+    @Test
+    public void shouldFailBcozWrongSequenceDates() {
+        final String input = "" +
+                "<avis:reelMetadata xmlns:avis=\"http://www.statsbiblioteket.dk/avisdigitalisering/microfilm/1/0/\">\n" +
+                "    <avis:titles>Adresse Contoirs Efterretninger</avis:titles>\n" +
+                "    <avis:startDate>1795-06-13</avis:startDate>\n" +
+                "    <avis:endDate>1790-06-15</avis:endDate>\n" +
+                "    <avis:batchIdFilmId>400022028241-1</avis:batchIdFilmId>\n" +
+                "    <avis:numberOfPictures>14</avis:numberOfPictures>\n" +
+                "    <avis:reductionRatio>15x</avis:reductionRatio>\n" +
+                "    <avis:captureResolutionOriginal measurement=\"pixels/inch\">400</avis:captureResolutionOriginal>\n" +
+                "    <avis:captureResolutionFilm measurement=\"pixels/inch\">6000</avis:captureResolutionFilm>\n" +
+                "    <avis:dateMicrofilmCreated>1970-06-05</avis:dateMicrofilmCreated>\n" +
+                "    <avis:looseLeavesFlag>true</avis:looseLeavesFlag>\n" +
+                "    <avis:boundVolumeFlag>false</avis:boundVolumeFlag>\n" +
+                "    <avis:resolutionOfDuplicateNegative>6.3</avis:resolutionOfDuplicateNegative>\n" +
+                "    <avis:resolutionCommentDuplicateNegative>No comments</avis:resolutionCommentDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.11</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.13</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:densityReadingDuplicateNegative>0.12</avis:densityReadingDuplicateNegative>\n" +
+                "    <avis:averageDensityDuplicateNegative>0.12</avis:averageDensityDuplicateNegative>\n" +
+                "    <avis:dminDuplicateNegative>0.12</avis:dminDuplicateNegative>\n" +
+                "</avis:reelMetadata>\n";
+        setUp();
+        handleTestEvent(input, resultCollector);
+        assertFalse(resultCollector.isSuccess(), resultCollector.toReport());
+    }
+
+    @Test
+    public void shouldFailBcozWrongDateFormat() {
+        final String input = "" +
+                "<avis:reelMetadata xmlns:avis=\"http://www.statsbiblioteket.dk/avisdigitalisering/microfilm/1/0/\">\n" +
+                "    <avis:titles>Adresse Contoirs Efterretninger</avis:titles>\n" +
+                "    <avis:startDate>1795/06/13</avis:startDate>\n" +
+                "    <avis:endDate>1795-06-15</avis:endDate>\n" +
+                "    <avis:batchIdFilmId>400022028241-1</avis:batchIdFilmId>\n" +
+                "    <avis:numberOfPictures>14</avis:numberOfPictures>\n" +
+                "    <avis:reductionRatio>15x</avis:reductionRatio>\n" +
+                "    <avis:captureResolutionOriginal measurement=\"pixels/inch\">400</avis:captureResolutionOriginal>\n" +
+                "    <avis:captureResolutionFilm measurement=\"pixels/inch\">6000</avis:captureResolutionFilm>\n" +
+                "    <avis:dateMicrofilmCreated>1970-06-05</avis:dateMicrofilmCreated>\n" +
+                "    <avis:looseLeavesFlag>true</avis:looseLeavesFlag>\n" +
+                "    <avis:boundVolumeFlag>false</avis:boundVolumeFlag>\n" +
                 "    <avis:resolutionOfDuplicateNegative>6.3</avis:resolutionOfDuplicateNegative>\n" +
                 "    <avis:resolutionCommentDuplicateNegative>No comments</avis:resolutionCommentDuplicateNegative>\n" +
                 "    <avis:densityReadingDuplicateNegative>0.11</avis:densityReadingDuplicateNegative>\n" +
