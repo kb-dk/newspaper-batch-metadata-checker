d405d365829f9e35b6fc17355da95d0fcce32930
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-29 15:36:02 +0100

    Include conf in classpath so that we read the logback.xml

--- src/main/scripts/pollAndWork.sh
+++ src/main/scripts/pollAndWork.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-java -classpath "$SCRIPT_DIR/../lib/*" dk.statsbiblioteket.newspaper.metadatachecker.MetadataChecker -c $SCRIPT_DIR/../conf/config.properties
+java -classpath "$SCRIPT_DIR/../conf:$SCRIPT_DIR/../lib/*" \
+   dk.statsbiblioteket.newspaper.metadatachecker.MetadataChecker -c $SCRIPT_DIR/../conf/config.properties
