0ed634f57b2dc843a36f52c2715b7805320d970f
jrg <jrg@statsbiblioteket.dk>
2014-08-14 12:57:42 +0200

    Added logging as review-followup

--- src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/caches/DocumentCache.java
+++ src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/caches/DocumentCache.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class DocumentCache {
     static Logger log = LoggerFactory.getLogger(DocumentCache.class);
 
-    public TimeSensitiveCache<String,Document> documentCache = new TimeSensitiveCache<>(100,true,10);
+    public TimeSensitiveCache<String,Document> documentCache = new TimeSensitiveCache<>(100,true,20);
 
     public DocumentCache() {
     }
@@ -20,6 +20,7 @@ public class DocumentCache {
             throws IOException {
         Document document = documentCache.get(event.getName());
         if (document == null) {
+            log.debug("Parsing event '{}' as DOM, because not found in cache", event.getName());
             document = DOM.streamToDOM(event.getData(), true);
             documentCache.put(event.getName(), document);
         }
