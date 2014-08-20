d166bb70e60d62bc15b5d0acb8b79ea6833d5517
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-08-20 12:44:16 +0200

    Much bigger cache to ensure that stuff is not unloaded before we are long gone

--- src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/caches/DocumentCache.java
+++ src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/caches/DocumentCache.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class DocumentCache {
     static Logger log = LoggerFactory.getLogger(DocumentCache.class);
 
-    public TimeSensitiveCache<String,Document> documentCache = new TimeSensitiveCache<>(10000,true,20);
+    public TimeSensitiveCache<String,Document> documentCache = new TimeSensitiveCache<>(10000,true,200);
 
     public DocumentCache() {
     }
@@ -20,10 +20,16 @@ public class DocumentCache {
             throws IOException {
         Document document = documentCache.get(event.getName());
         if (document == null) {
-            log.debug("Parsing event '{}' as DOM, because not found in cache", event.getName());
-            document = DOM.streamToDOM(event.getData(), true);
-            documentCache.put(event.getName(), document);
+            document = parseDocument(event);
         }
+        return document;
+    }
+
+    private Document parseDocument(AttributeParsingEvent event) throws IOException {
+        Document document;
+        log.debug("Parsing event '{}' as DOM, because not found in cache", event.getName());
+        document = DOM.streamToDOM(event.getData(), true);
+        documentCache.put(event.getName(), document);
         return document;
     }
 
