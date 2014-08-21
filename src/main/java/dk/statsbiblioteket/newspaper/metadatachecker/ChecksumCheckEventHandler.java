96a01f8da703b8f0ded221e17d59616fa062f9b9
jrg <jrg@statsbiblioteket.dk>
2014-08-21 12:41:16 +0200

    Class for calculating checksums for all files

---
+++ src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/ChecksumCheckEventHandler.java
@@ -0,0 +1,72 @@
+package dk.statsbiblioteket.newspaper.metadatachecker;
+
+import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.common.AttributeParsingEvent;
+import dk.statsbiblioteket.medieplatform.autonomous.iterator.eventhandlers.DefaultTreeEventHandler;
+import dk.statsbiblioteket.util.Bytes;
+import dk.statsbiblioteket.util.Checksums;
+import dk.statsbiblioteket.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+public class ChecksumCheckEventHandler extends DefaultTreeEventHandler {
+    private ResultCollector resultCollector;
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private static final String CHECKSUM = "checksumChecker";
+
+
+    /**
+     * Constructor for this class.
+     *
+     * @param resultCollector The result collector to collect errors in
+     */
+    public ChecksumCheckEventHandler(ResultCollector resultCollector) {
+        this.resultCollector = resultCollector;
+    }
+
+    @Override
+    public void handleAttribute(AttributeParsingEvent attributeEvent) {
+        String existingChecksum;
+        try {
+            existingChecksum = attributeEvent.getChecksum();
+        } catch (IOException e) {
+            log.warn("Error getting checksum in {}", attributeEvent.getName(), e);
+            resultCollector.addFailure(attributeEvent.getName(), CHECKSUM, getClass().getSimpleName(),
+                    "2F-O1: Error getting checksum: " + e.toString(),
+                    Strings.getStackTrace(e));
+            return;
+        }
+
+        String calculatedChecksum;
+        try {
+            calculatedChecksum = calculateChecksum(attributeEvent.getData());
+        } catch (IOException e) {
+            log.warn("Error calculating checksum on data in {}", attributeEvent.getName(), e);
+            resultCollector.addFailure(attributeEvent.getName(), CHECKSUM, getClass().getSimpleName(),
+                    "2F-O1: Error calculating checksum on data: " + e.toString(),
+                    Strings.getStackTrace(e));
+            return;
+        }
+
+        if (!calculatedChecksum.equalsIgnoreCase(existingChecksum)) {
+            log.debug("Expected checksum {}, but was {} in {}", existingChecksum, calculatedChecksum,
+                    attributeEvent.getName());
+            resultCollector.addFailure(attributeEvent.getName(), CHECKSUM, getClass().getSimpleName(),
+                    "2F-O1: Checksum mismatch. Value in md5-file: " + existingChecksum + "; calculated checksum: "
+                            + calculatedChecksum);
+        }
+    }
+
+    private String calculateChecksum(InputStream stream) throws IOException {
+        return Bytes.toHex(Checksums.md5(stream));
+    }
+
+    @Override
+    public void handleFinish() {
+        resultCollector.setTimestamp(new Date());
+    }
+
+}
