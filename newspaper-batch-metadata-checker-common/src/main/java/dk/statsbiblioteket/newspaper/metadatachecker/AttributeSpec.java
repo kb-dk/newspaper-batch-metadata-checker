dc4ae93d58f5e49d37c53041acc408a78b58d829
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-08-11 13:38:59 +0200

    Javadoc'ed AttributeSpec

--- newspaper-batch-metadata-checker-common/src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/AttributeSpec.java
+++ newspaper-batch-metadata-checker-common/src/main/java/dk/statsbiblioteket/newspaper/metadatachecker/AttributeSpec.java
@@ -1,7 +1,9 @@
 package dk.statsbiblioteket.newspaper.metadatachecker;
 
 /**
- * Created by abr on 8/7/15.
+ * This is the config for the more generic checkers, namely the SchematronValidatorEventHandler and the SchemaValidatorEventHandler
+ * @see SchematronValidatorEventHandler
+ * @see SchemaValidatorEventHandler
  */
 public class AttributeSpec {
 
@@ -11,6 +13,14 @@ public class AttributeSpec {
     private String messagePrefix;
     private String type;
 
+    /**
+     * Create a new attribute spec
+     * @param postfix Every file with this postfix will be validated with the event handlers
+     * @param xsdFile the Xml Schema to validate the file against. If null, no schema validation will be done
+     * @param schematronFile the Schematron document to validate the file against. If null, no schematron validation will be done
+     * @param messagePrefix For schema validation, this is the prefix to use when reporting errors.
+     * @param type The type of error, usually "metadata" or "jp2file", but can be whatever
+     */
     public AttributeSpec(String postfix, String xsdFile, String schematronFile, String messagePrefix, String type) {
         this.postfix = postfix;
         this.xsdFile = xsdFile;
@@ -19,22 +29,42 @@ public class AttributeSpec {
         this.type = type;
     }
 
+    /**
+     * The postfix for the files to be validated with the specification in this AttributeSpec
+     * @return
+     */
     public String getPostfix() {
         return postfix;
     }
 
+    /**
+     * The Xml Schema file path to the schema to validate against
+     * @return
+     */
     public String getXsdFile() {
         return xsdFile;
     }
 
+    /**
+     * The Schematron file path to the schematron document to validate against
+     * @return
+     */
     public String getSchematronFile() {
         return schematronFile;
     }
 
+    /**
+     * The prefix for the schema, but not schematron, errors reported
+     * @return
+     */
     public String getMessagePrefix() {
         return messagePrefix;
     }
 
+    /**
+     * The type of errors reported. Should correspond to the type of file being validated.
+     * @return
+     */
     public String getType() {
         return type;
     }
